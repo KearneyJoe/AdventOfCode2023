3f972000869c9d71c6abdccbe524ac4fb356bfc4
KearneyJoe <44685213+KearneyJoe@users.noreply.github.com>
2023-12-09 16:55:50 -0500

    Day 7 & 8 AoC

--- Day07/day07.js
+++ Day07/day07.js
@@ -1,7 +1,266 @@
 const fs = require("fs");
 
 const data = fs
-  //.readFileSync("day07_actual.txt", { encoding: "utf-8" })
-  .readFileSync("day07_example.txt", { encoding: "utf-8" })
+  .readFileSync("day07_actual.txt", { encoding: "utf-8" })
+  // .readFileSync("day07_example.txt", { encoding: "utf-8" })
   .split("\n")
-  .map((x) => parseInt(x));
+// .map((x) => parseInt(x));
+
+// const cardStrength = {
+//   'A': 13,
+//   'K': 12,
+//   'Q': 11,
+//   'J': 10,
+//   'T': 9,
+//   '9': 8,
+//   '8': 7,
+//   '7': 6,
+//   '6': 5,
+//   '5': 4,
+//   '4': 3,
+//   '3': 2,
+//   '2': 1
+// }
+
+const cardStrengthLetters = {
+  'A': 'A',
+  'K': 'B',
+  'Q': 'C',
+  'J': 'D',
+  'T': 'E',
+  '9': 'F',
+  '8': 'G',
+  '7': 'H',
+  '6': 'I',
+  '5': 'J',
+  '4': 'K',
+  '3': 'L',
+  '2': 'M'
+}
+
+const handStrength = {
+  'five-of-a-kind': 7,
+  'four-of-a-kind': 6,
+  'full-house': 5,
+  'three-of-a-kind': 4,
+  'two-pair': 3,
+  'one-pair': 2,
+  'high-card': 1
+}
+
+function parseCardHand(hand) {
+  let handObj = {}
+  let replacedHand = ''
+  for (let i = 0; i < hand.length; i++) {
+    let card = hand[i]
+    if (!handObj[card]) {
+      handObj[card] = 1
+    } else {
+      handObj[card] += 1
+    }
+    replacedHand += cardStrengthLetters[card]
+  }
+
+  let handLabel;
+  let values = Object.values(handObj)
+  if (values.indexOf(5) != -1) {
+    handLabel = 'five-of-a-kind'
+  } else if (values.indexOf(4) != -1) {
+    handLabel = 'four-of-a-kind'
+  } else if (values.indexOf(3) != -1 && values.indexOf(2) != -1) {
+    handLabel = 'full-house'
+  } else if (values.indexOf(3) != -1) {
+    handLabel = 'three-of-a-kind'
+  } else {
+    let pairCounter = 0
+    for (let j = 0; j < values.length; j++) {
+      if (values[j] === 2) {
+        pairCounter += 1
+      }
+    }
+    switch (pairCounter) {
+      case 2:
+        handLabel = 'two-pair'
+        break;
+      case 1:
+        handLabel = 'one-pair'
+        break;
+      default:
+        handLabel = 'high-card'
+        break;
+    }
+  }
+  return { 'hand': hand, 'handLabel': handLabel, 'handLetters': replacedHand }
+}
+
+
+function sortByKey(data, key) {
+  return Object.fromEntries(
+    Object.entries(data).sort((a, b) => a[1][key].localeCompare(b[1][key]))
+  );
+}
+
+function part1(data) {
+  let hands = {}
+  for (let i = 0; i < data.length; i++) {
+    let hand = data[i].split(" ")[0]
+    let bet = data[i].split(" ")[1]
+    let parsedHand = parseCardHand(hand)
+    hands[hand] = { ...parsedHand }
+    hands[hand]['bet'] = parseInt(bet)
+  }
+
+  let sorted = []
+  for (let hand of Object.keys(hands)) {
+    sorted.push([hand, hands[hand]['handLetters'], hands[hand]['handLabel'], hands[hand]['bet']])
+  }
+
+  sorted.sort((a, b) => a[1].localeCompare(b[1]));
+  let rank = sorted.length;
+  let betTotal = 0;
+  //loopthrough
+  for (let handType of Object.keys(handStrength)) {
+    for (let hand of sorted) {
+      if (hand[2] === handType) {
+        betTotal += hand[3] * rank
+        rank--
+      }
+    }
+  }
+  return betTotal
+}
+
+console.log("Part 1: ", part1(data)); //Part 1: 251106089
+
+const cardStrengthLettersV2 = {
+  'A': 'A',
+  'K': 'B',
+  'Q': 'C',
+  'T': 'E',
+  '9': 'F',
+  '8': 'G',
+  '7': 'H',
+  '6': 'I',
+  '5': 'J',
+  '4': 'K',
+  '3': 'L',
+  '2': 'M',
+  'J': 'Z',
+}
+
+function parseCardHandV2(hand) {
+  let handObj = {}
+  let replacedHand = ''
+  let jCounter = 0;
+  for (let i = 0; i < hand.length; i++) {
+    let card = hand[i]
+    if (card === 'J') {
+      jCounter++;
+    } else {
+      if (!handObj[card]) {
+        handObj[card] = 1
+      } else {
+        handObj[card] += 1
+      }
+    }
+    replacedHand += cardStrengthLettersV2[card]
+  }
+
+  let handLabel;
+  let values = Object.values(handObj)
+  if (values.indexOf(5) != -1) {
+    handLabel = 'five-of-a-kind'
+  } else if (values.indexOf(4) != -1) {
+    handLabel = 'four-of-a-kind'
+  } else if (values.indexOf(3) != -1 && values.indexOf(2) != -1) {
+    handLabel = 'full-house'
+  } else if (values.indexOf(3) != -1) {
+    handLabel = 'three-of-a-kind'
+  } else {
+    let pairCounter = 0
+    for (let j = 0; j < values.length; j++) {
+      if (values[j] === 2) {
+        pairCounter += 1
+      }
+    }
+    switch (pairCounter) {
+      case 2:
+        handLabel = 'two-pair'
+        break;
+      case 1:
+        handLabel = 'one-pair'
+        break;
+      default:
+        handLabel = 'high-card'
+        break;
+    }
+  }
+
+  if (jCounter === 1) {
+    if (handLabel === 'high-card') {
+      handLabel = 'one-pair'
+    } else if (handLabel === 'one-pair') {
+      handLabel = 'three-of-a-kind'
+    } else if (handLabel === 'two-pair') {
+      handLabel = 'full-house'
+    } else if (handLabel === 'three-of-a-kind') {
+      handLabel = 'four-of-a-kind'
+    } else if (handLabel === 'four-of-a-kind') {
+      handLabel = 'five-of-a-kind'
+    }
+  } else if (jCounter === 2) {
+    if (handLabel === 'high-card') {
+      handLabel = 'three-of-a-kind'
+    } else if (handLabel === 'one-pair') {
+      handLabel = 'four-of-a-kind'
+    } else if (handLabel === 'three-of-a-kind') {
+      handLabel = 'five-of-a-kind'
+    }
+  } else if (jCounter === 3) {
+    if (handLabel === 'high-card') {
+      handLabel = 'four-of-a-kind'
+    } else if (handLabel === 'one-pair') {
+      handLabel = 'five-of-a-kind'
+    }
+  } else if (jCounter === 4) {
+    if (handLabel === 'high-card') {
+      handLabel = 'five-of-a-kind'
+    }
+  } else if (jCounter === 5) {
+    handLabel = 'five-of-a-kind'
+  }
+
+  return { 'hand': hand, 'handLabel': handLabel, 'handLetters': replacedHand }
+}
+
+function part2(data) {
+  let hands = {}
+  for (let i = 0; i < data.length; i++) {
+    let hand = data[i].split(" ")[0]
+    let bet = data[i].split(" ")[1]
+    let parsedHand = parseCardHandV2(hand)
+    hands[hand] = { ...parsedHand }
+    hands[hand]['bet'] = parseInt(bet)
+  }
+
+  let sorted = []
+  for (let hand of Object.keys(hands)) {
+    sorted.push([hand, hands[hand]['handLetters'], hands[hand]['handLabel'], hands[hand]['bet']])
+  }
+
+  sorted.sort((a, b) => a[1].localeCompare(b[1]));
+  let rank = sorted.length;
+  let betTotal = 0;
+
+  for (let handType of Object.keys(handStrength)) {
+    for (let hand of sorted) {
+      if (hand[2] === handType) {
+        betTotal += hand[3] * rank
+        rank--
+      }
+    }
+  }
+  return betTotal
+}
+
+console.log("Part 2: ", part2(data)); //Part 2: 249620106
--- Day08/day08.js
+++ Day08/day08.js
@@ -1,7 +1,104 @@
 const fs = require("fs");
 
 const data = fs
-  //.readFileSync("day08_actual.txt", { encoding: "utf-8" })
-  .readFileSync("day08_example.txt", { encoding: "utf-8" })
+  .readFileSync("day08_actual.txt", { encoding: "utf-8" })
+  // .readFileSync("day08_example.txt", { encoding: "utf-8" })
   .split("\n")
-  .map((x) => parseInt(x));
+// .map((x) => parseInt(x));
+
+
+function part1(data) {
+  let instructions = data[0]
+
+  let mapObj = {}
+  for (let i = 2; i < data.length; i++) {
+    let split = data[i].split(" ")
+    let key = split[0]
+    let left = split[2].slice(1, 4)
+    let right = split[3].slice(0, 3)
+    mapObj[key] = { 'left': left, 'right': right }
+  }
+
+  let nextKey = 'AAA'
+  let counter = 0
+
+  while (nextKey != 'ZZZ') {
+    let direction = instructions[counter]
+    let obj = mapObj[nextKey]
+    if (direction === 'L') {
+      nextKey = obj['left']
+    } else {
+      nextKey = obj['right']
+    }
+
+    counter++
+    if (counter >= instructions.length) {
+      instructions += instructions
+    }
+  }
+
+  return counter
+}
+
+console.log("Part 1: ", part1(data)) //Part 1: 18023
+
+
+function findLCM(numbers) {
+  // Function to calculate the greatest common divisor (GCD)
+  const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+
+  // Function to calculate the least common multiple (LCM)
+  const lcm = (a, b) => (a * b) / gcd(a, b);
+
+  // Function to find the LCM of an array of numbers
+  const findArrayLCM = (arr) => arr.reduce((acc, num) => lcm(acc, num), 1);
+
+  return findArrayLCM(numbers);
+}
+
+function part2(data) {
+
+  let instructions = data[0]
+  let startingNodes = [];
+
+  let mapObj = {}
+  for (let i = 2; i < data.length; i++) {
+    let split = data[i].split(" ")
+    let key = split[0]
+    if (key[2] === 'A') {
+      startingNodes.push(key)
+    }
+    let left = split[2].slice(1, 4)
+    let right = split[3].slice(0, 3)
+    mapObj[key] = { 'left': left, 'right': right }
+  }
+
+  let distances = [];
+
+  for (let start of startingNodes) {
+    let counter = 0
+    let nextKey = start
+    while (!nextKey.endsWith('Z')) {
+      let direction = instructions[counter]
+      let obj = mapObj[nextKey]
+
+      if (direction === 'L') {
+        nextKey = obj['left']
+      } else {
+        nextKey = obj['right']
+      }
+
+      counter++
+      if (counter >= instructions.length) {
+        instructions += instructions
+      }
+    }
+
+    distances.push(counter)
+
+  }
+
+  return findLCM(distances);
+}
+
+console.log("Part 2: ", part2(data)) //Part 2: 14449445933179
