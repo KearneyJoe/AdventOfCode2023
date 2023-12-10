9125b1a200fefca7d4748e320fafca815dd54d3f
KearneyJoe <44685213+KearneyJoe@users.noreply.github.com>
2023-12-10 16:42:31 -0500

    Day 9 AoC

--- Day09/day09.js
+++ Day09/day09.js
@@ -1,7 +1,78 @@
 const fs = require("fs");
 
 const data = fs
-  //.readFileSync("day09_actual.txt", { encoding: "utf-8" })
-  .readFileSync("day09_example.txt", { encoding: "utf-8" })
+  .readFileSync("day09_actual.txt", { encoding: "utf-8" })
+  // .readFileSync("day09_example.txt", { encoding: "utf-8" })
   .split("\n")
-  .map((x) => parseInt(x));
+// .map((x) => parseInt(x));
+
+function part1(data) {
+
+  let histories = []
+  for (let history of data) {
+    histories.push(history.split(" "))
+  }
+
+  let totals = 0
+  for (let history of histories) {
+    totals += processHistory(history)
+  }
+
+  return totals
+}
+
+function processHistory(history) {
+
+  if (new Set(history).size === 1) {
+    return history[0]
+  }
+
+  let diffs = []
+  let lastNum = parseInt(history[history.length - 1])
+  for (let i = 0; i < history.length - 1; i++) {
+    diffs.push(parseInt(history[i + 1]) - parseInt(history[i]))
+  }
+
+  let num = lastNum + processHistory(diffs)
+
+  return num
+}
+
+console.log("Part 1: ", part1(data)) // Part 1: 1901217887
+
+
+function part2(data) {
+
+  let histories = []
+  for (let history of data) {
+    histories.push(history.split(" "))
+  }
+
+  let totals = 0
+  for (let history of histories) {
+    totals += processHistoryBackwards(history)
+  }
+
+  return totals
+}
+
+function processHistoryBackwards(history) {
+
+  if (new Set(history).size === 1) {
+    return history[0]
+  }
+
+  let diffs = []
+  let firstNum = parseInt(history[0])
+  for (let i = 0; i < history.length - 1; i++) {
+    diffs.push(parseInt(history[i + 1]) - parseInt(history[i]))
+  }
+
+  let num = firstNum - processHistoryBackwards(diffs)
+
+  return num
+}
+
+
+
+console.log("Part 2: ", part2(data)) // Part 2: 905
--- Day10/day10.js
+++ Day10/day10.js
@@ -4,4 +4,39 @@ const data = fs
   //.readFileSync("day10_actual.txt", { encoding: "utf-8" })
   .readFileSync("day10_example.txt", { encoding: "utf-8" })
   .split("\n")
-  .map((x) => parseInt(x));
+// .map((x) => parseInt(x));
+
+// console.log(data)
+
+
+function part1(data) {
+  data = cleanData(data)
+  let start = findStart(data)
+
+}
+
+function cleanData(data) {
+  let cleaned = []
+  for (let row of data) {
+    cleaned.push(row.split(''))
+  }
+  return cleaned
+}
+
+function findStart(data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      if (data[i][j] === 'S') {
+        return [i, j]
+      }
+    }
+  }
+  return null
+}
+
+function journey(start, data) {
+
+  return null
+}
+
+console.log("Part 1: ", part1(data)) //Part 1: 
